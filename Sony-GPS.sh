83f7d4b31b2c06cdce730a31db3bf6a864dbe55f
Paul Schaap <poorlymac@gmail.com>
2020-01-12 11:12:35 +1100

    Better methodology to join

--- Sony-GPS.sh
+++ Sony-GPS.sh
@@ -9,42 +9,6 @@
 #           - Convert GPS from NMEA to more common GPX
 ################################################################################
 
-function write_file {
-	cd "$MP4GPS"
-	if [ ${#PMP4[@]} -eq 1 ]
-	then
-		# if there is only 1 softlink
-		echo Linking ${PMP4[@]} to $PGPS.MP4
-		#echo "ln -s $PMP4 $PGPS.MP4"
-		ln -s ../$PMP4 $PGPS.MP4
-	else
-		# More than one we need to losslessly join
-		echo Writing ${PMP4[@]} to $PGPS.MP4
-		I=0
-		FILES=()
-		for P4 in "${PMP4[@]}"
-		do
-			I=$((I+1))
-			TS="/tmp/Sony-GPS${I}.ts"
-			FILES+=($TS)
-			# Convert to TransportStream
-			"$FFMPEG" -y -i "../${P4}" -c copy -bsf:v h264_mp4toannexb -f mpegts $TS
-		done
-		# Join TrnasportStreams
-		"$FFMPEG" -y -i "concat:$(IFS=\|; echo "${FILES[*]}")" -c copy -bsf:a aac_adtstoasc $PGPS.MP4
-		# Cleanup
-		rm -f /tmp/Sony-GPS*.ts
-	fi
-	ln -s ../PRIVATE/SONY/GPS/$PGPS.LOG .
-	# Convert NMEA to GPX if we have GPSBabel and the LOG exists
-	if [ "$GPSBABEL" != "" ] && [ -f "$PGPS.LOG" ]
-	then
-		"$GPSBABEL" -i nmea -f $PGPS.LOG  -o gpx,gpxver=1.1 -F $PGPS.gpx
-	fi
-	# Return back to directory
-	cd -
-}
-
 MISSING=0
 # Check for ffmpeg
 FFMPEG="`which ffmpeg`"
@@ -53,7 +17,7 @@ then
 	echo "ERROR: ffmpeg missing, brew install ffmpeg"
 	MISSING=1
 fi
-# Check for ffmpeg
+# Check for exiftool
 EXIFTOOL="`which exiftool`"
 if [ ! -f "$EXIFTOOL" ]
 then
@@ -65,14 +29,47 @@ then
 	exit
 fi
 
+function find_log {
+	GPS=""
+	for LOG in $(ls $1/PRIVATE/SONY/GPS/*.LOG)
+	do
+		DATE=$(head -1 $LOG | awk -F\/ '{print $4}')
+		if [ "$2" == "$DATE" ]
+		then
+			GPS=$LOG
+		fi
+	done
+}
+
+function transcode {
+	TSC=${#TS[@]}
+	if [ $TSC -gt 1 ]
+	then
+		rm -f MP4GPS/${NF}_$(basename $PRV_GPS | sed 's/\.[^.]*$//').mp4
+		FILES=()
+		for (( i=0; i<$TSC; i++ ))
+		do
+				FILENAME=${TS[$i]}
+				echo "Transcoding $FILENAME ..."
+				TSF="/tmp/Sony-GPS${i}.ts"
+				FILES+=($TSF)
+				"$FFMPEG" -y -hide_banner -loglevel panic -y -i "$FILENAME" -c copy -bsf:v h264_mp4toannexb -f mpegts $TSF
+		done
+		# Join TrnasportStreams
+		"$FFMPEG" -hide_banner -loglevel panic -y -i "concat:$(IFS=\|; echo "${FILES[*]}")" -c copy -bsf:a aac_adtstoasc "${NF}_$(basename $PRV_GPS | sed 's/\.[^.]*$//').MP4"
+		mv "${NF}_$(basename $PRV_GPS | sed 's/\.[^.]*$//').MP4" MP4GPS
+		# Cleanup
+		rm -f /tmp/Sony-GPS*.ts
+	fi
+}
+
 # Check if we are in the correct directory
-MP4GPS="MP4GPS"
-if [ -d "MP_ROOT" ] && [ -d "PRIVATE/SONY/GPS" ]
+if [ "$(ls */MP_ROOT)" != "" ] && [ "$(ls */PRIVATE/SONY/GPS)" != "" ]
 then
-	echo "Creating linked Videos (or joined) and GPS in $MP4GPS"
-	mkdir -p "$MP4GPS"
+	mkdir -p MP4GPS
 else
-	echo "Usage $0 from the Sony storage root, i.e. where MP_ROOT and PRIVATE are."
+	echo "Usage $0 from one directory above the Sony storage roots, i.e. where there are MP_ROOT and PRIVATE subdirectories."
+	exit
 fi
 
 # Look for GPSBabel
@@ -91,80 +88,41 @@ else
 	echo "GPSBabel not found, not converting to GPX"
 fi
 
-MP4S=(`ls MP_ROOT/*/*.MP4`)
-MP4C=${#MP4S[@]}
-echo "Found $MP4C MP4's"
-F=0
-PART=1
-for MP4 in "${MP4S[@]}"
+N=0
+for MP4 in $(ls */MP_ROOT/*/*.MP4)
 do
-	F=$((F + 1))
-
-	# Determine Start
-	CD=`exiftool -CreateDate ${MP4} -d "%y%m%d:%H:%M:%S" 2>/dev/null`
-	if [ $? -eq 0 ]
+	CD=$("$EXIFTOOL" -CreateDate $MP4 | grep "^Create Date")
+	Y4=${CD:34:4}
+	Y2=${CD:36:2}
+	MN=${CD:39:2}
+	DY=${CD:42:2}
+	HH=${CD:45:2}
+	MM=${CD:48:2}
+	SS=${CD:51:2}
+	find_log "$(echo "$MP4" | awk -F "/" '{print $1}')" "$Y4$MN$DY$HH$MM$SS.000"
+	if [ "$GPS" == "" ]
 	then
-		# Turn Start time into seconds
-		DY=`echo ${CD} | sed 's/ //g' | awk -F\: '{print $2}'`
-		HR=`echo ${CD} | sed 's/ //g' | awk -F\: '{print $3}'`
-		MN=`echo ${CD} | sed 's/ //g' | awk -F\: '{print $4}'`
-		SC=`echo ${CD} | sed 's/ //g' | awk -F\: '{print $5}'`
-		START_SECONDS=$((10#$HR * 60 * 60 + 10#$MN * 60 + 10#$SC))
-
-		# Check to see if this is a run on from the last file
-		WRITE_PREVIOUS=0
-		if [ "$DY" == "$PDY" ]
-		then
-			DF=$((START_SECONDS - PEND))
-			# Check the gap between this and the next recording
-			if [ $DF -le 3 ] && [ $DF -ge -3 ]
-			then
-				# The same recording if 3 seconds difference
-				PART=$((PART + 1))
-				LMP4=(${PMP4[@]})
-				LMP4+=($MP4)
-			else
-				# A new recording
-				LMP4=($MP4)
-				WRITE_PREVIOUS=1
-				PART=1
-				G=$((G + 1))
-			fi
-		else
-			# A new recording
-			LMP4=($MP4)
-			WRITE_PREVIOUS=1
-			PART=1
-			G=0
-		fi
-
-		# Write Previous
-		if [ $WRITE_PREVIOUS -eq 1 ] && [ $F -ne 1 ]
-		then
-			write_file
-		fi
-
-		# Determine End
-		DR=`exiftool -n -Duration "${MP4}" | sed 's/ //g'`
-		SC=`echo ${DR} | awk -F[\:,\.] '{print $2}'`
-		END_SECONDS=$((START_SECONDS + $SC))
-
-		# Check for GPS
-		GPS=`printf "${DY}%02X" $G`
-		if [ -f "PRIVATE/SONY/GPS/$GPS.LOG" ]
-		then
-			echo ${MP4} ${CD} for ${DR} [$DY/$START_SECONDS to $END_SECONDS] part $PART GPS $GPS
-		else
-			echo ${MP4} ${CD} for ${DR} [$DY/$START_SECONDS to $END_SECONDS] part $PART GPS $GPS MISSING
-		fi
-
-		# Set next values
-		PDY=$DY
-		PEND=$END_SECONDS
-		PGPS=$GPS
-		PMP4=(${LMP4[@]})
+		TS+=("$MP4")
+		echo "$MP4 $PRV_GPS <- Continuation"
 	else
-		echo $MP4 file invalid
+		transcode
+		if [ $(wc -l <"$GPS" | awk '{print $1}') -gt 10 ]
+		then
+			N=$((N+2))
+			NF=$(printf "%03d" $N)
+			TS=("$MP4")
+			echo "Copying $MP4 and $GPS ..."
+			rsync --progress $MP4 MP4GPS/${NF}_$(basename $GPS | sed 's/\.[^.]*$//').mp4
+			cp $GPS MP4GPS/${NF}_$(basename $GPS | sed 's/\.[^.]*$//').nmea
+			# Convert NMEA to GPX if we have GPSBabel and the LOG exists
+			if [ "$GPSBABEL" != "" ]
+			then
+				"$GPSBABEL" -i nmea -f $GPS -o gpx,gpxver=1.1 -F MP4GPS/${NF}_$(basename $GPS | sed 's/\.[^.]*$//').gpx
+			fi
+			PRV_GPS=$GPS
+		else
+			echo "Ignoring $MP4 and $GPS ..."
+		fi
 	fi
 done
-write_file
+transcode
